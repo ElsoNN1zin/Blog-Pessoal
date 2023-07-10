6049fbf49316ceb5f20e6737fac2e103a3b4809f
ElsoNN1zin <elson3031@hotmail.com>
2023-07-10 17:35:22 -0300

    Novas atualizaçoes

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -16,7 +19,10 @@ import jakarta.validation.constraints.Size;
 @Table (name= "tb_postagens")
 public class Postagem {
 	
-	 @Id
+		@UpdateTimestamp
+		private LocalDateTime data;
+
+		@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 
@@ -27,8 +33,64 @@ public class Postagem {
 	    @NotBlank
 	    @Size(min = 10, max = 1000)
 	    private String texto;
+	    
+	    @ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Tema tema;
+	    
+		@ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Usuario usuario;
 
-	    @UpdateTimestamp
-	    private LocalDateTime data;
+	    public Tema getTema() {
+			return tema;
+		}
+
+		public void setTema(Tema tema) {
+			this.tema = tema;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public String getTexto() {
+			return texto;
+		}
+
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+
+		public LocalDateTime getData() {
+			return data;
+		}
+
+		public void setData(LocalDateTime data) {
+			this.data = data;
+		}
+
+		public Usuario getUsuario() {
+			return usuario;
+		}
+
+		public void setUsuario(Usuario usuario) {
+			this.usuario = usuario;
+		}
+
+
+		
 
 }
